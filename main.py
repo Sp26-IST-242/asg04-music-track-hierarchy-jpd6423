42ac6c288b85bf74b28ee615a7da307434f471c1
jpd6423 <jpd6423@psu.edu>
2026-03-19 10:55:25 -0400

    Coded main.py with prints

--- main.py
+++ main.py
@@ -18,3 +18,42 @@ After sorting:
 (Sarah Koenig, Journalism) Serial active = False,  debut year: 2014, duration: 01:30:00 is explicit: False
 (Kendrick Lamar, Hip-Hop) DAMN. active = True,  debut year: 2017, duration: 03:40
 """
+
+from artist import Artist
+from album import Album
+from music_track import MusicTrack
+from song import Song
+from podcast import Podcast
+from playlist import Playlist
+
+def main():
+    ken = Song(Artist("Kendrick Lamar","Hip-Hop"),
+           Album("DAMN.",True,list(range(2017,2018))),
+           220)
+    
+    ala = Song(Artist("Alanis Morissette","Alternative"),
+           Album("Jagged Little Pill",False,list(range(1995,1996))),
+           245)
+    
+    joe = Podcast(Artist("Joe Rogan","Comedy"),
+           Album("The Joe Rogan Experience",True,list(range(2009,2010))),
+           9000, True)
+    
+    sar = Podcast(Artist("Sarah Koenig","Journalism"),
+           Album("Serial",False,list(range(2014,2015))),
+           5400)
+    
+    p = Playlist()
+    p.add_track(ken)
+    p.add_track(ala)
+    p.add_track(joe)
+    p.add_track(sar)
+
+    print("Before sorting:")
+    print(p)
+    p.sort_by_release_year()
+    print("\nAfter sorting:")
+    print(p)
+
+if __name__ == "__main__":
+    main()
